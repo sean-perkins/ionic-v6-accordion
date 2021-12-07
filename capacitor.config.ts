f7f34b0a8f5de750cb306a0f01ea06101f2f4b31
Sean Perkins <sean@ionic.io>
2021-12-07 17:09:25 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'accordion-v6-example',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
